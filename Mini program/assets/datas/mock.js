a8d71ed025826e2b1a036cca84e44b441c502e2f
fu <43463006@qq.com>
2018-06-14 11:05:28 +0800

    调整订单页布局

--- Mini program/assets/datas/mock.js
+++ Mini program/assets/datas/mock.js
@@ -7,24 +7,27 @@ module.exports = {
   ],
   orderList:[
     {
-      orderNum:111111,
+      orderNum: 11111111111111,
       orderTime: '2018-05 - 20 20:20',
       orderCarts: [
-        { id: 1, title: '精品午间套餐', num: 5, price: 75.00.toFixed(2) },
-         { id: 2, title: '驴肉火烧', num: 3, price: 45.00.toFixed(2) },
-        { id: 3, title: '小卖店', num: 1, price: 15.00.toFixed(2) },
-        { id: 4, title: '三秦套餐', num: 2, price: 15.00.toFixed(2) }
+        { id: 1, title: '小卖店', num: 1, price: 15.00.toFixed(2) },
+        { id: 2, title: '三秦套餐三秦套餐', num: 3, price: 15.00.toFixed(2) },
+        { id: 3, title: '小卖店', num: 4, price: 45.00.toFixed(2) },
+        { id: 4, title: '三秦套餐', num: 5, price: 15.00.toFixed(2) },
+        { id: 5, title: '小卖店', num: 1, price: 15.00.toFixed(2) }
       ],
       orderStatus: '订单完成',
       orderTotalNum: 15,
       orderTotalPrice: 60
     },
     {
-      orderNum: 22222,
+      orderNum: 22221111111111,
       orderTime: '2018-06 - 20 20:20',
       orderCarts: [
         { id: 1, title: '驴肉火烧', num: 1, price: 15.00.toFixed(2) },
         { id: 2, title: '三秦套餐', num: 1, price: 15.00.toFixed(2) },
+        { id: 5, title: '小卖店', num: 3, price: 15.00.toFixed(2) },
+        { id: 5, title: '小卖店', num: 3, price: 15.00.toFixed(2) }
       ],
       orderStatus: '订单完成',
       orderTotalNum: 2,
