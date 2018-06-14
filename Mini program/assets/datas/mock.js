2e06adad529b18ebe268a520c0b360aceb75d31c
fu <43463006@qq.com>
2018-06-14 19:30:26 +0800

    订单页增加下拉加载更多

--- Mini program/assets/datas/mock.js
+++ Mini program/assets/datas/mock.js
@@ -5,7 +5,7 @@ module.exports = {
     { id: 3, title: '小卖店', num: 0, price: 15.00.toFixed(2), max: 4 },
     { id: 4, title: '三秦套餐', num: 0, price: 15.00.toFixed(2), max: 7 },
   ],
-  orderList:[
+  orderListPage1:[
     {
       orderNum: '12345678901234567890',
       orderTime: '2018-05-20 20:20:20',
@@ -24,7 +24,6 @@ module.exports = {
       orderCarts: [
         { id: 1, title: '驴肉火烧', num: 1, price: 15.00.toFixed(2) },
         { id: 2, title: '驴肉火烧', num: 1, price: 15.00.toFixed(2) },
-
       ],
       orderStatus: '订单完成',
       orderTotalNum: 2,
@@ -50,21 +49,39 @@ module.exports = {
       orderTotalNum: 2,
       orderTotalPrice: 10
     },
+
+   
+  ],
+
+  orderListPage2: [
     {
-      orderNum: 55555555555555,
+      orderNum: '12345678901234567890',
+      orderTime: '2018-05-20 20:20:20',
+      orderCarts: [
+        { id: 1, title: '我是page2', num: 1, price: 1500.00.toFixed(2) },
+        { id: 2, title: '三秦套餐三秦套餐三秦套餐三秦套餐三秦套餐三秦套餐', num: 300, price: 15000.00.toFixed(2) },
+        { id: 3, title: '小卖店', num: 40, price: 45.00.toFixed(2) },
+      ],
+      orderStatus: '订单完成',
+      orderTotalNum: 15,
+      orderTotalPrice: 600.00.toFixed(2)
+    },
+    {
+      orderNum: 22222222,
       orderTime: '2018-06 - 20 20:20',
       orderCarts: [
-        { id: 1, title: '驴肉火烧', num: 1, price: 15.00.toFixed(2) },
+        { id: 1, title: '我是page2', num: 1, price: 15.00.toFixed(2) },
+        { id: 2, title: '驴肉火烧', num: 1, price: 15.00.toFixed(2) },
       ],
       orderStatus: '订单完成',
       orderTotalNum: 2,
-      orderTotalPrice: 10
+      orderTotalPrice: 1.00.toFixed(2)
     },
     {
-      orderNum: 6666666666666,
+      orderNum: 3333333333,
       orderTime: '2018-06 - 20 20:20',
       orderCarts: [
-        { id: 1, title: '驴肉火烧', num: 1, price: 15.00.toFixed(2) },
+        { id: 1, title: '我是page2', num: 1, price: 15.00.toFixed(2) },
       ],
       orderStatus: '订单完成',
       orderTotalNum: 2,
--- Mini program/pages/user/orderlist/orderlist.js
+++ Mini program/pages/user/orderlist/orderlist.js
@@ -7,61 +7,30 @@ Page({
    * 页面的初始数据
    */
   data: {
-    orderPayList: []
+    orderPayList: [],
+    height:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.getSystemInfo({
+      success: (res) => {
+        this.setData({
+          height: res.windowHeight * 2-160
+        })
+      },
+    })
     //载入订单数据
     this.setData({
-      orderPayList: datas.orderList
+      orderPayList: datas.orderListPage1
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  lower(){
+   
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
   /**
    * 用户点击右上角分享
    */
