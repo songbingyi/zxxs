6ff3d0b0b0e7e44a2d44a7d3dad1dd6001afff94
fu <43463006@qq.com>
2018-06-16 13:59:36 +0800

    首次登录授权逻辑

--- Mini program/pages/index/index.js
+++ Mini program/pages/index/index.js
@@ -6,42 +6,46 @@ var orderService = require('../../service/order-http.service.js')
 Page({
   data: {
     userInfo: {},
-    hasUserInfo: false,
+    hasUserInfo: true,
     indexLogo: "http://218.244.158.175/static/zuoxiang/images/logo_indexbg.png",
   },
   onLoad: function() {
-
+    wx.showLoading({
+      title: '加载中..',
+    })
     //判断用户是否已经同意获取信息
     wx.getSetting({
-      success: (res) => {
-        if (!res.authSetting['scope.userInfo']) { //已经授权，不需要弹窗
-          this.setData({
-            hasUserInfo: true
+      success:(res) => {
+        wx.hideLoading()
+        if (res.authSetting['scope.userInfo']){ //如果已经同意过授权
+          wx.getStorage({
+            key: 'userInfo',
+            success: (res) => {
+              this.setData({ //从缓存里读取userInfo
+                hasUserInfo:true,
+                userInfo:res.data
+              })
+            },
           })
-        } else { //没有授权，需要弹窗
+        }else{
           this.setData({
-            hasUserInfo: false
+            hasUserInfo:false
           })
         }
-      },
-      fail: function() {
-        wx.showToast({
-          title: '系统提示:网络错误',
-          icon: 'warn',
-          duration: 1500,
-        })
-      },
-    })
+        
+      }
+    })    
 
+    //微信登录
     wx.login({
       success: (res) => {
-
         var code = res.code;
         if (code) {
           wx.request({
             url: 'https://api.weixin.qq.com/sns/jscode2session' + '?appid=wx8bf90b8b9fbcf28e' + '&secret=e5bd94eb0ba0e5a99304fac78a3f065e' + '&js_code=' + code + '&grant_type=authorization_code',
             success: (e) => {
-              console.log(e)
+               console.log(e)
+
             }
           })
         }
@@ -119,72 +123,78 @@ Page({
     })
   },
 
-  // data: {
-  //   motto: 'Hello World',
-  //   userInfo: {},
-  //   hasUserInfo: false,
-  //   canIUse: wx.canIUse('button.open-type.getUserInfo')
-  // },
-  // //事件处理函数
-  // bindViewTap: function() {
-  //   wx.navigateTo({
-  //     url: '../logs/logs'
-  //   })
-  // },
-  // onLoad: function () {
-  //   if (app.globalData.userInfo) {
-  //     this.setData({
-  //       userInfo: app.globalData.userInfo,
-  //       hasUserInfo: true
-  //     })
-  //   } else if (this.data.canIUse){
-  //     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-  //     // 所以此处加入 callback 以防止这种情况
-  //     app.userInfoReadyCallback = res => {
-  //       this.setData({
-  //         userInfo: res.userInfo,
-  //         hasUserInfo: true
-  //       })
-  //     }
-  //   } else {
-  //     // 在没有 open-type=getUserInfo 版本的兼容处理
-  //     wx.getUserInfo({
-  //       success: res => {
-  //         app.globalData.userInfo = res.userInfo
-  //         this.setData({
-  //           userInfo: res.userInfo,
-  //           hasUserInfo: true
-  //         })
-  //       }
-  //     })
-  //   }
-  // },
-  // getUserInfo: function(e) {
-  //   console.log(e)
-  //   app.globalData.userInfo = e.detail.userInfo
-  //   this.setData({
-  //     userInfo: e.detail.userInfo,
-  //     hasUserInfo: true
-  //   })
-  // }
-  // getinfo:function(e){
-  //   orderService.getMemberDetail((d) => {
 
-  //   });
-  // }
 
-  agreeGetUser(e) {
-    if (e.detail.userInfo) {
+  agreeGetUser(e) { //点击授权按钮
+    console.log(e.detail)
+    if (e.detail.userInfo) { //用户点击同意授权
+      this.setData({
+        hasUserInfo: true,
+        userInfo: e.detail.userInfo
+      })
       wx.showLoading({
         title: '加载中...',
-        duration: 800,
-      })
-      this.setData({
-        hasUserInfo: false,
-        userInfo: e.detail
+        duration: 600,
       })
 
+      wx.setStorage({
+        key: 'userInfo',
+        data: e.detail.userInfo,
+      })
     }
-
   }
 })
+
+// data: {
+//   motto: 'Hello World',
+//   userInfo: {},
+//   hasUserInfo: false,
+//   canIUse: wx.canIUse('button.open-type.getUserInfo')
+// },
+// //事件处理函数
+// bindViewTap: function() {
+//   wx.navigateTo({
+//     url: '../logs/logs'
+//   })
+// },
+// onLoad: function () {
+//   if (app.globalData.userInfo) {
+//     this.setData({
+//       userInfo: app.globalData.userInfo,
+//       hasUserInfo: true
+//     })
+//   } else if (this.data.canIUse){
+//     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+//     // 所以此处加入 callback 以防止这种情况
+//     app.userInfoReadyCallback = res => {
+//       this.setData({
+//         userInfo: res.userInfo,
+//         hasUserInfo: true
+//       })
+//     }
+//   } else {
+//     // 在没有 open-type=getUserInfo 版本的兼容处理
+//     wx.getUserInfo({
+//       success: res => {
+//         app.globalData.userInfo = res.userInfo
+//         this.setData({
+//           userInfo: res.userInfo,
+//           hasUserInfo: true
+//         })
+//       }
+//     })
+//   }
+// },
+// getUserInfo: function(e) {
+//   console.log(e)
+//   app.globalData.userInfo = e.detail.userInfo
+//   this.setData({
+//     userInfo: e.detail.userInfo,
+//     hasUserInfo: true
+//   })
+// }
+// getinfo:function(e){
+//   orderService.getMemberDetail((d) => {
+
+//   });
+// }
